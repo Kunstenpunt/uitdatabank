939ecb55363d0c8762de8062b47debab7754f358
Tom Ruette <tom.ruette@sirris.be>
2016-07-29 10:28:06 +0200

    work with a config file, rather than strings in the code

--- uitdatabank.py
+++ uitdatabank.py
@@ -2,12 +2,18 @@ import requests
 from codecs import open
 from json import loads, dumps
 from requests_oauthlib import OAuth1
+from configparser import ConfigParser
 
 
 class UiTdatabank():
-    def __init__(self, app_key, app_secret, user_token, user_secret, url):
-        self.auth = OAuth1(app_key, app_secret, user_token, user_secret)
-        self.url = url
+    def __init__(self, settings_file="settings.cfg"):
+        settings = ConfigParser()
+        settings.read(settings_file)
+        self.auth = OAuth1(settings["oauth"]["app_key"],
+                           settings["oauth"]["app_secret"],
+                           settings["oauth"]["user_token"],
+                           settings["oauth"]["user_secret"])
+        self.url = settings["uitdatabank"]["url"]
         self.headers = {'Accept': 'application/json'}
 
     def find(self, organiser_label=None):
@@ -19,8 +25,7 @@ class UiTdatabank():
         return loads(r.text)
 
 if __name__ == '__main__':
-    ud = UiTdatabank('BAAC107B-632C-46C6-A254-13BC2CE19C6C', 'ec9a0e8c2cdc52886bc545e14f888612', '', '',
-                     'https://www.uitid.be/uitid/rest/searchv2/search')
+    ud = UiTdatabank()
     flagey = ud.find(organiser_label="Flagey")
     with open("flagey.json", "w", "utf-8") as f:
         f.write(dumps(flagey, indent=2))
