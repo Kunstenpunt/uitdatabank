bf3c06b7262261ed833b5a6b2451caa51aa53740
Tom Ruette <tom.ruette@sirris.be>
2016-08-01 14:11:24 +0200

    some early documentation

--- uitdatabank.py
+++ uitdatabank.py
@@ -9,6 +9,11 @@ from os.path import dirname
 
 class UiTdatabankSearchResults():
     def __init__(self, results_string):
+        """
+        Initializes the uitdatabank search results
+        :param results_string: the json output of the uitdatabank json
+        :return: None
+        """
         self.results = loads(results_string)
 
     @staticmethod
@@ -50,9 +55,19 @@ class UiTdatabank():
             self.supported_query_parameter_fields = [item.strip() for item in f.readlines()]
 
     def find(self, params):
+        """
+        Main find method that makes the actual api call
+        :param params: the full query, containing the q, fq, etc. fields
+        :return: An uitdatabank searchresults object
+        """
         return UiTdatabankSearchResults(requests.get(self.url, auth=self.auth, params=params, headers=self.headers).text)
 
     def construct_query_parameters(self, kwargs):
+        """
+        Validates the query parameter fields, and constructs a query that can be send to the API
+        :param kwargs: a dictionary containing all the parameters for the query, e.g. {"q": "city:Brussels", "fq":"type:event"}
+        :return: a dictionary of parameters that can be sent to the API, e.g. using the find() method
+        """
         out = {}
         for key, value in kwargs.items():
             if key in self.supported_query_parameter_fields:
@@ -63,6 +78,12 @@ class UiTdatabank():
 
     @staticmethod
     def __construct_query(supported_fields, key_value_tuples_with_booleans=list):
+        """
+        Validates a specific type of query against a list of supported fields
+        :param supported_fields: a list of fields that is supported in the given type of query
+        :param key_value_tuples_with_booleans: a list of (key, value) tuples, potentially with booleans in between, that will be rewritten to a query
+        :return: a string that can be passed to "q" in the api call
+        """
         if len(key_value_tuples_with_booleans) % 2 == 0:
             raise ValueError("Not a correct query")
         else:
@@ -79,9 +100,19 @@ class UiTdatabank():
             return q
 
     def construct_production_query(self, key_value_tuples_with_booleans=list):
+        """
+        Construct a query for a production
+        :param key_value_tuples_with_booleans: a list of fields that is supported in the given type of query
+        :return: (a string that can be passed to "q" in the api call, a string that can be passed to "fq" in the api call)
+        """
         return self.__construct_query(self.supported_production_query_fields, key_value_tuples_with_booleans), "type:production"
 
     def construct_event_query(self, key_value_tuples_with_booleans=list):
+        """
+        Construct a query for events
+        :param key_value_tuples_with_booleans: a list of fields that is supported in the given type of query
+        :return: (a string that can be passed to "q" in the api call, a string that can be passed to "fq" in the api call)
+        """
         return self.__construct_query(self.supported_event_query_fields, key_value_tuples_with_booleans), "type:event"
 
     def find_upcoming_events_by_organiser_label(self, organiser_label):
