db45017ec69a32be7cb46c5fdba765ab41e239a3
Tom Ruette <tom.ruette@sirris.be>
2016-08-05 10:49:40 +0200

    this is a bit more future proof

--- uitdatabank/shortcuts.py
+++ uitdatabank/shortcuts.py
@@ -5,7 +5,7 @@ class Shortcuts(UiTdatabank):
     def __init__(self, path_to_settings_file, test):
         UiTdatabank.__init__(self, path_to_settings_file, test)
 
-    def find_upcoming_events_by_organiser_label(self, organiser_label):
-        q, fq = self.construct_event_query([("organiser_label", organiser_label)])
+    def __find_upcoming_events_by_x(self, x, value):
+        q, fq = self.construct_event_query([(x, value)])
         params = self.construct_parameters_for_api_call({'q': q, 'fq': fq, 'rows': 10 if self.__test else 10000, 'past': False})
         return self.find(params)
